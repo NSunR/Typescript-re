f24d7c592220e3710cb3bb4d0bde188c6e40a5b7
HaeRheeNam <hanm3208@gmail.com>
2023-04-18 00:33:43 +0900

    docs : 보기 편하게 코드 올림

--- using-ts.js
+++ using-ts.js
@@ -4,6 +4,27 @@ var input2 = document.getElementById("num2");
 function add(num1, num2) {
   return num1 + num2;
 }
-butoon.addEventListener("click", function () {
-  console.log(add(input1.value, input2.value));
+button.addEventListener("click", function () {
+  console.log(add(+input1.value, +input2.value));
 });
+
+// var button = document.querySelector("button");
+// var input1 = document.getElementById("num1"); //2. 유형 변환도 사라졌어.
+// var input2 = document.getElementById("num2");
+// function add(num1, num2) {
+//   //1. 타입들이 사라졌음을 알 수 있고
+//   return num1 + num2;
+// }
+// butoon.addEventListener("click", function () {
+//   console.log(add(+input1.value, +input2.value));
+// });
+// //3. 바닐라 자바스크립트만 남았어.
+// var button = document.querySelector("button");
+// var input1 = document.getElementById("num1");
+// var input2 = document.getElementById("num2");
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+// button.addEventListener("click", function () {
+//   console.log(add(+input1.value, +input2.value));
+// });
--- using-ts.ts
+++ using-ts.ts
@@ -1,3 +1,27 @@
+const button = document.querySelector("button");
+const input1 = document.getElementById("num1")! as HTMLInputElement;
+const input2 = document.getElementById("num2")! as HTMLInputElement;
+
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+button.addEventListener("click", function () {
+  console.log(add(+input1.value, +input2.value));
+});
+
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
+
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+
+// button.addEventListener("click", function () {
+//   console.log(add(+input1.value, +input2.value));
+// });
+
 //js-only.js 복사본으로 시작
 //수정할 에러가 즉시 반환될 거야.
 // 이게 올바른 IDE를 사용하는 경우에 얻을 수 있는 타입스크립트의 큰 장점 중 하나야.
@@ -89,14 +113,14 @@
 //이를 통해서 타입 문자열의 인수를 숫자 타입의 매개변수에
 //할당할 수 없음을 알 수 있어.
 
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
 
-function add(num1: number, num2: number) {
-  return num1 + num2;
-}
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
 
-button.addEventListener("click", function () {
-  console.log(add(+input1.value, +input2.value));
-});
+// button.addEventListener("click", function () {
+//   console.log(add(+input1.value, +input2.value));
+// });
